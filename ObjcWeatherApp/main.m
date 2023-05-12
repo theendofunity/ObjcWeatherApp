ed1b3b0e41245be73f24cd127316c5d588c22f03
Dmitriy Dudkin <d.dudkin@growapp.biz>
2023-05-12 17:26:46 +0400

    Initial Commit

---
+++ ObjcWeatherApp/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ObjcWeatherApp
+//
+//  Created by Дмитрий Дудкин on 12.05.2023.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
