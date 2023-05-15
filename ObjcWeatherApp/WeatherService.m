1ff7c1fdaa0e582bbf5796bb9213f90bce3f2036
Dmitriy Dudkin <d.dudkin@growapp.biz>
2023-05-15 11:39:09 +0400

    get weather begin

---
+++ ObjcWeatherApp/WeatherService.m
@@ -0,0 +1,18 @@
+//
+//  WeatherService.m
+//  ObjcWeatherApp
+//
+//  Created by Дмитрий Дудкин on 12.05.2023.
+//
+
+#import <Foundation/Foundation.h>
+#import "WeatherService.h"
+
+@implementation WeatherService
+
+
+- (NSString *)getWeather:(NSString *)city { 
+    return @"some weather";
+}
+
+@end
--- ObjcWeatherApp/main.m
+++ ObjcWeatherApp/main.m
@@ -6,11 +6,35 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "WeatherService.h"
+
+void getWeather(void);
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        while (YES) {
+            getWeather();
+        }
+        
     }
     return 0;
+}
+
+void getWeather(void) {
+    NSLog(@"Enter city name");
+    
+    char cityName[56];
+    
+    scanf("%s", cityName);
+    
+    NSLog(@"Your choice");
+    NSLog(@"%s", cityName);
+    
+    NSString *cityNameString = [[NSString alloc] initWithBytes:cityName length:strlen(cityName) encoding:NSUTF8StringEncoding];
+
+    WeatherService *service = [[WeatherService alloc] init];
+    
+    NSString * weather = [service getWeather:cityNameString];
+    
+    NSLog(@"%@", weather);
 }
